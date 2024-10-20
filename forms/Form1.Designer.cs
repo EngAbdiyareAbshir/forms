63b8a63e1003cc195f91ed16e92631ef20bedfee
Abdi-yare <Abdi-yare@DESKTOP-6M13JLN>
2024-10-20 02:57:44 -0800

    Add project files.

---
+++ forms/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace forms
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Cyan;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(99, 123);
+            label1.Name = "label1";
+            label1.Size = new Size(83, 32);
+            label1.TabIndex = 0;
+            label1.Text = "label1";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Fuchsia;
+            button1.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(81, 289);
+            button1.Name = "button1";
+            button1.Size = new Size(136, 50);
+            button1.TabIndex = 1;
+            button1.Text = "Italy";
+            button1.TextAlign = ContentAlignment.TopCenter;
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Fuchsia;
+            button2.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.Location = new Point(314, 299);
+            button2.Name = "button2";
+            button2.Size = new Size(186, 51);
+            button2.TabIndex = 2;
+            button2.Text = "Soomaaliya";
+            button2.TextAlign = ContentAlignment.TopCenter;
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.AutoSize = true;
+            button3.BackColor = Color.Fuchsia;
+            button3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button3.Location = new Point(609, 308);
+            button3.Name = "button3";
+            button3.Size = new Size(112, 42);
+            button3.TabIndex = 3;
+            button3.Text = "UK";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+    }
+}
---
+++ forms/Form1.cs
@@ -0,0 +1,25 @@
+namespace forms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = ("caasimada Italy waa Roma");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = ("caasimada soomaaliya waa Mogadisho");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Text = ("caasimada UK waa landhan");
+        }
+    }
+}
